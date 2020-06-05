aa29921976ac2934baa6edf35b2bf2c235ceb3e7
anliwei <155177343@qq.com>
2020-06-05 21:44:44 +0800

    1.0.0

---
+++ src/main/java/com/ncell/wangcai/WangcaiApplication.java
@@ -0,0 +1,13 @@
+package com.ncell.wangcai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangcaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WangcaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ncell/wangcai/WangcaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncell.wangcai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WangcaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
