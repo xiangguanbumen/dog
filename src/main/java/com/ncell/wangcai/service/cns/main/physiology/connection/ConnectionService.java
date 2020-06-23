1bdd3d014d7c42a92f6a80dcc66dd4653876cf8d
anliwei <155177343@qq.com>
2020-06-23 19:50:42 +0800

    GitHub

---
+++ src/main/java/com/ncell/wangcai/service/cns/main/physiology/connection/ConnectionService.java
@@ -0,0 +1,8 @@
+package com.ncell.wangcai.service.cns.main.physiology.connection;
+
+/**
+ * @author anliwei
+ * @Date 2020/6/23 19:49
+ */
+public interface ConnectionService {
+}
---
+++ src/main/java/com/ncell/wangcai/service/cns/main/physiology/connection/impl/ConnectionServiceImpl.java
@@ -0,0 +1,10 @@
+package com.ncell.wangcai.service.cns.main.physiology.connection.impl;
+
+import com.ncell.wangcai.service.cns.main.physiology.connection.ConnectionService;
+
+/**
+ * @author anliwei
+ * @Date 2020/6/23 19:50
+ */
+public class ConnectionServiceImpl implements ConnectionService {
+}
